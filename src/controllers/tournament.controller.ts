43638feec1c6d56c82f67ef10a4dedf3c3135e75
Ivona Bušljeta <92543364+IvonaBusljeta@users.noreply.github.com>
2023-10-30 06:02:33 +0100

    error handling

--- src/controllers/tournament.controller.ts
+++ src/controllers/tournament.controller.ts
@@ -15,10 +15,13 @@ const {v4: uuidv4} = require('uuid');
 
 export async function getUserTournaments(req, res) {
     const user = req.oidc.user.name;
-    const finishedTournaments = await getAllFinishedTournamentsByCreator(user);
-    const unfinishedTournaments = await getAllUnfinishedTournamentsByCreator(user);
-
-    res.render('user_tournaments', {user, finishedTournaments, unfinishedTournaments});
+    try {
+        const finishedTournaments = await getAllFinishedTournamentsByCreator(user);
+        const unfinishedTournaments = await getAllUnfinishedTournamentsByCreator(user);
+        res.render('user_tournaments', {user, finishedTournaments, unfinishedTournaments});
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export async function saveTournament(req, res) {
@@ -32,23 +35,27 @@ export async function saveTournament(req, res) {
         tie: scores[1],
         defeat: scores[2]
     } as TournamentDTO;
-    const tournament_id = await saveTournamentByCreator(tournament, user);
 
-    const names = players.split(';');
-    const player_ids = await saveAllPlayersByTournament(names, tournament_id);
+    try {
+        const tournament_id = await saveTournamentByCreator(tournament, user);
 
-    const matches = generateSchedule(player_ids);
-    await saveAllMatchesByTournament(matches, tournament_id);
+        const names = players.split(';');
+        const player_ids = await saveAllPlayersByTournament(names, tournament_id);
 
-    const code = uuidv4();
-    cache.put(code, {creator: user, tournament_id: tournament_id});
+        const matches = generateSchedule(player_ids);
+        await saveAllMatchesByTournament(matches, tournament_id);
 
+        const code = uuidv4();
+        cache.put(code, {creator: user, tournament_id: tournament_id});
 
-    res.redirect(`/tournaments/${code}`);
+        res.redirect(`/tournaments/${code}`);
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export async function getTournamentMatches(req, res) {
-    let user : string | undefined;
+    let user: string | undefined;
     if (req.oidc.isAuthenticated()) {
         user = req.oidc.user?.name;
     }
@@ -58,12 +65,15 @@ export async function getTournamentMatches(req, res) {
     const isCreator = user == creator;
     const share_link = `${req.protocol}://${req.get('host')}/share/${code}`;
 
-    const tournament = await getTournamentById(tournament_id);
-    const stats = (await getAllPlayersByTournamentWithStats(tournament_id))
-        .sort((a, b) => b.score - a.score);
-    const matches = await getAllMatchesByTournament(tournament_id);
-
-    res.render('tournament_matches', {user, code, tournament, share_link, stats, matches, isCreator})
+    try {
+        const tournament = await getTournamentById(tournament_id);
+        const stats = (await getAllPlayersByTournamentWithStats(tournament_id))
+            .sort((a, b) => b.score - a.score);
+        const matches = await getAllMatchesByTournament(tournament_id);
+        res.render('tournament_matches', {user, code, tournament, share_link, stats, matches, isCreator})
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function updateTournamentStats(req, res) {
@@ -76,7 +86,11 @@ export async function updateTournamentStats(req, res) {
         order: order
     } as MatchKey;
 
-    await updateWinnerByKey(key, winner);
+    try {
+        await updateWinnerByKey(key, winner);
+        res.redirect(`/tournaments/${code}`);
+    } catch (error) {
+        console.log(error);
+    }
 
-    res.redirect(`/tournaments/${code}`);
 }
